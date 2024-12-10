ab4117720a9ee4555d155bc04cdedffa269f2921
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2024-12-10 18:10:33 +0600

    project ended

--- src/component/Content/Content.jsx
+++ src/component/Content/Content.jsx
@@ -4,7 +4,7 @@ const Content = () => {
         <div>
              <div className="justify-center text-center">
             <h1 className="font-bold text-5xl tex p-5 mt-5">Our Receipes</h1>
-            <p className=" text-2xl p-5">Discover a world of flavors with our carefully curated recipes. From simple delights to gourmet creations, each dish is crafted to inspire and satisfy. Let the adventure begin in your kitchen!</p>
+            <p className=" text-2xl p-5">Discover a world of flavors with our carefully curated recipes.From simple delights to gourmet creations, each dish is crafted to inspire and satisfy. Let the adventure begin in your kitchen!</p>
         </div>
         </div>
     );
