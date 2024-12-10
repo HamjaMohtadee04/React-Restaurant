caf1fc5e360a7ef6be09b80581a1bdaa30c431ed
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2024-12-10 10:57:26 +0600

    toast functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,30 @@ import CookTable from './component/CookTable/CookTable'
 import Header from './component/Header/Header'
 import Nav from './component/Nav/Nav'
 import Receipes from './component/Receipes/Receipes'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const  [cookTable,setCookTable] = useState([])
   const [currentlyCooking, setCurrentlyCooking] = useState([]);
 
-  const handleAddtoCookTable = (receipe) =>{
-   const newCookTable = [...cookTable,receipe]
-   setCookTable(newCookTable)
-   //remove recipe_id
-  //  const remainingReceipe = cookTable.filter(cookTable => cookTable.recipe_id !==recipe_id)
-  //  setCookTable(remainingReceipe)
-  // 
+  const handleAddtoCookTable = (recipe) =>{
+  //  const newCookTable = [...cookTable,receipe]
+  //  setCookTable(newCookTable)
+  //  //remove recipe_id
+  // //  const remainingReceipe = cookTable.filter(cookTable => cookTable.recipe_id !==recipe_id)
+  // //  setCookTable(remainingReceipe)
+  // // 
+   // Check if the recipe is already in the "Want to Cook" table
+   const isAlreadyAdded = cookTable.some((item) => item.recipe_id === recipe.recipe_id);
+
+   if (isAlreadyAdded) {
+     // Show a toast notification if the recipe already exists
+     toast.error("This recipe is already added to Want to Cook!");
+   } else {
+     // Add the recipe if it doesn't exist
+     const newCookTable = [...cookTable, recipe];
+     setCookTable(newCookTable);
+   }
   }
   
 
@@ -40,6 +53,7 @@ function App() {
 </div>
 
       </div>
+      <ToastContainer position="top-right" autoClose={3000} />
     </>
   )
 }
